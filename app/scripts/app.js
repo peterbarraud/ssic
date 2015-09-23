fea343a153b8072b13bed192b06fbbeb4e6872b4
peter barraud <gapeterb@gmail.com>
2015-09-23 07:39:22 +0530

    first commit

---
+++ app/scripts/app.js
@@ -0,0 +1,12 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name schoolSiteApp
+ * @description
+ * # schoolSiteApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('schoolSiteApp', []);
